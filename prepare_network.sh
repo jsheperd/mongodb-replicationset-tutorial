26e0e34990640a0411c17a932da509c4ee778b21
János <juhasz.janos.flutemaker@gmail.com>
2017-03-22 23:08:19 +0100

    Update prepare_network.sh

--- prepare_network.sh
+++ prepare_network.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Create some new interfaces to run the instances on different ip adresses
 ifconfig enp0s25:0 192.168.0.110/24
 ifconfig enp0s25:1 192.168.0.111/24
 ifconfig enp0s25:2 192.168.0.112/24
