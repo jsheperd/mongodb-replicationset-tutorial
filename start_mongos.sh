a7787cfb95e96dfca0cecab99e6200f3ec621af0
jshepherd <juhasz.janos.flutemaker@gmail.com>
2017-03-23 15:25:16 +0100

    Correct

--- start_mongos.sh
+++ start_mongos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # start three instances to shape a replicationset
-xterm -hold -e 'mongod --config db01.config' &
-xterm -hold -e 'mongod --config db02.config' &
-xterm -hold -e 'mongod --config db03.config' &
+mongod --config db01.config &
+mongod --config db02.config &
+mongod --config db03.config &
