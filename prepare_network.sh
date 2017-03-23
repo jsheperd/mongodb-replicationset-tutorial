58de4aa12b56d5ee0627bd9b169dfb9bade0f94c
jshepherd <juhasz.janos.flutemaker@gmail.com>
2017-03-23 09:03:35 +0100

    missed mod

--- prepare_network.sh
+++ prepare_network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Create some new interfaces to run the instances on different ip adresses
-ifconfig lo:1 127.0.0.11/24
-ifconfig lo:2 127.0.0.12/24
-ifconfig lo:3 127.0.0.13/24
+ifconfig lo:1 10.0.0.1/24
+ifconfig lo:2 10.0.0.2/24
+ifconfig lo:3 10.0.0.3/24
